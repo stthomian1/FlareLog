91053b880e621763878f5c51b802c8bc964a3c17
stthomian1 <stthomian1@gmail.com>
2026-08-06 15:43:04 -0400

    Update FlareLogCore package platform to iOS 17 and set CODE_SIGN_STYLE to Manual in project

--- FlareLogCore/Package.swift
+++ FlareLogCore/Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let package = Package(
     name: "FlareLogCore",
     platforms: [
-        .iOS(.v16),
-        .macOS(.v13)
+        .iOS(.v17),
+        .macOS(.v14)
     ],
     products: [
         .library(
